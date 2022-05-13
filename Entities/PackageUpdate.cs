391abd6b5383e720c47839923d1f51e4d2559953
Samuel Nascimento <samuelpn15@gmail.com>
2022-05-13 14:42:41 -0300

    Adicionando classes, atributos das classes e construtores

---
+++ Entities/PackageUpdate.cs
@@ -0,0 +1,17 @@
+﻿namespace DevTrackR.Entities
+{
+    public class PackageUpdate
+    {
+        public PackageUpdate(string status)
+        {
+            Status = status;
+            UpdateDate = DateTime.Now;
+        }
+
+        public int Id { get; private set; }
+
+        public string Status { get; private set; }
+
+        public DateTime UpdateDate { get; private set; }
+    }
+}
